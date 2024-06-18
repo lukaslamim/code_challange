f17f0a9cb94ec4e4ba347eb317785a578a17b978
lucas lamim <lukas.lamim@hotmail.com>
2024-06-18 10:42:20 -0300

    spring commit
    inicial spring config

---
+++ src/test/java/codechalange/codechalange/CodeChalangeApplicationTests.java
@@ -0,0 +1,13 @@
+package codechalange.codechalange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChalangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
